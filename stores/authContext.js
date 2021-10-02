0b970dabfe5446b660030318e9f0627a07c0f1f8
Cipppp <pirvuciprian37@gmail.com>
2021-10-02 20:16:46 +0300

    login & logout

---
+++ stores/authContext.js
@@ -0,0 +1,54 @@
+import { createContext, useState, useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
+
+const AuthContext = createContext({
+    user: null,
+    login: () => {},
+    logout: () => {},
+    authReady: false,
+});
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        netlifyIdentity.on('login', (user) => {
+            setUser(user);
+            netlifyIdentity.close();
+            console.log('login event');
+        });
+
+        netlifyIdentity.on('logout', () => {
+            setUser(null);
+            console.log('logout event');
+        });
+
+        return () => {
+            netlifyIdentity.off('login');
+            netlifyIdentity.off('logout');
+        };
+
+        // Init netlify identity connection
+        netlifyIdentity.init();
+    }, []);
+
+    const login = () => {
+        netlifyIdentity.open();
+    };
+
+    const logout = () => {
+        netlifyIdentity.logout();
+    };
+
+    const context = {
+        user,
+        login,
+        logout,
+    };
+
+    return (
+        <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
